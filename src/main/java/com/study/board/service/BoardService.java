e83200196ef41262b307f48fd823ccd5c62ca005
sosow0212 <sosow0212@naver.com>
2021-09-30 17:39:39 +0900

    게시글 삭제

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -21,12 +21,22 @@ public class BoardService {
 
     // 게시글 리스트 처리
     public List<Board> boardList() {
+
         return boardRepository.findAll();
     }
 
 
     // 특정 게시글 불러오기
     public Board boardView(Integer id) {
+
         return boardRepository.findById(id).get();
     }
+
+
+    // 특정 게시글 삭제
+    public void boardDelete(Integer id) {
+
+        boardRepository.deleteById(id);
+    }
+
 }
