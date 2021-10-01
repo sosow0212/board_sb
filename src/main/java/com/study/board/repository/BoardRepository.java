882f649348b3584e5f2ae26583515638a0dcbe87
sosow0212 <sosow0212@naver.com>
2021-10-01 22:28:45 +0900

    index.html css추가

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Integer> { // Board entity에서 id 지정하는 타입이 Integer라서 Integer을 넣음
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+    // Board entity에서 id 지정하는 타입이 Integer라서 Integer을 넣음
 
 }
